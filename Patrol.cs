559896281a694c953433e5b8a65a313909bd8780
Pingplof <60694913+Pingplof@users.noreply.github.com>
2020-05-19 12:05:19 -0300

    Add files via upload

---
+++ Patrol.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+
+    public float speed;
+    private float waitTime;
+    public float startWaitTime; 
+
+    public Transform[] moveSpot;
+    private int randonSpot;
+
+    void Start()
+    {
+        waitTime = startWaitTime; 
+        randonSpot = Random.Range(0,moveSpot.Length);
+    }
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, moveSpot[randonSpot].position, speed * Time.deltaTime);
+        if(Vector3.Distance(transform.position,moveSpot[randonSpot].position)<0.2f)
+        {
+            if(waitTime <= 0)
+            {
+                randonSpot = Random.Range(0, moveSpot.Length);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+}
---
+++ Spawn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public Transform[] spawnSpot; 
+    private int randonSpawn;
+    void Start()
+    {
+        randonSpawn = Random.Range(0, spawnSpot.Length);
+        transform.position = spawnSpot[randonSpawn].position;
+    }
+}
